ac25279681aa3e8cc9ad68cee82813c230ac09e9
Diego CG <diego.cerongutierrez@gmail.com>
2021-01-08 20:24:11 -0600

    useForm agregado

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { useForm } from '../hooks/useForm'
 
 export const LoginForm = () => {
+
+    const [ { email, password }, handleInputChange ] = useForm({
+        email: '',
+        password: ''
+    });
+
     return (
         <form className='form'>
-            <input className='form-input' placeholder='Email' />
-            <input className='form-input' placeholder='Password' />
+            <input 
+                className='form-input' 
+                placeholder='Email' 
+                name='email'
+                type='email'
+                value={email}
+                onChange={handleInputChange}
+                />
+            <input 
+                className='form-input' 
+                placeholder='Password'
+                name='password'
+                type='password'
+                value={password}
+                onChange={handleInputChange} 
+                />
             <button>Login</button>
         </form>
 
