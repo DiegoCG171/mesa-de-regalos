6580806b958476f24d1e2c547f5b8e168fb16a5b
Diego CG <diego.cerongutierrez@gmail.com>
2021-01-09 23:49:04 -0600

    Main creado

--- src/pages/main/MainScreen.js
+++ src/pages/main/MainScreen.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { FaGifts } from 'react-icons/fa';
+import { AiFillPlusCircle} from 'react-icons/ai'
+import { Link } from 'react-router-dom';
 
 export const MainScreen = () => {
 
-    const {name} = useSelector(state => state.auth)
+    const { name } = useSelector(state => state.auth)
 
     return (
-        <div >
-            <h1>¡Bienvenido {name} a tu Mesa de Regalos!</h1>
+        <div className='main'>
+            <div className='main-container'>
+                <div className='main-logo'>
+                    MR
+                <FaGifts className='main-icon'/>
+                </div>
+                <header className='main-header'>
+                    <h1>¡Bienvenido {name} a tu Mesa de Regalos!</h1>
+                </header>
+                <div className='main-buttons'>
+                    <Link to='/' className='button-main'>Crear Wish List <AiFillPlusCircle className='btn-icon' /></Link>
+                    <Link to='/' className='button-main'>Ver mis Wish List's</Link> 
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/pages/wishListPages/WishListScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const WishListScreen = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
